67b44e04093f8c4528ecc2f0ed8406823f6fd18a
Hamza-Khan-03 <hamzakhan102003@gmail.com>
2024-07-15 10:15:40 +0500

    Added data table
    inserted data table in accounts page

--- src/app/(dashboard)/accounts/page.tsx
+++ src/app/(dashboard)/accounts/page.tsx
@@ -1,11 +1,43 @@
 "use client"
-
+import { Button } from "@/components/ui/button"
+import {Card,CardContent,CardHeader, CardTitle} from "@/components/ui/card"
+import { Plus } from "lucide-react"
 import React from 'react'
+import { columns,Payment } from "./columns"
+import { DataTable } from "@/components/data-table"
 
+const data:Payment[] = [
+  {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+]
 function AccountsPage() {
+  // const newAccount = useNewAccount();
   return (
-    <div>
-      <h1>Accounts Page</h1>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">
+    Accounts page
+          </CardTitle> 
+          {/* <Button onClick={newAccount.onOpen} size={"sm"}>  later 3:33:00*/}
+          <Button size={"sm"}>
+            <Plus className="size-4 mr-2">
+              Add New
+            </Plus> 
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            filterkey="email"
+            columns={columns}
+            data={data} />
+        </CardContent>
+      </Card>
+      {/* <h1>Accounts Page</h1> */} 
     </div>
   )
 }
