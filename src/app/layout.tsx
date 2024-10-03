cb026e37f951d455121d7c898ad70ff3b94e1280
Muhammad-Hamza-Khan-03 <hamzakhan102003@gmail.com>
2024-10-03 19:18:57 +0500

    added mobile view navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dashboard ",
+  title: "Insight AI Dashboard ",
   description: "Final Year Project",
 };
 
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,7 +1,15 @@
 "use client";
-
+import {Sheet,
+    SheetContent,
+    SheetTrigger
+} from "@/components/ui/sheet"
+import {useMedia} from 'react-use'
 import { usePathname } from "next/navigation";
 import NavButton from "./nav-button";
+import { useState } from "react";
+import  {useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 const routes = [
     {
@@ -41,7 +49,55 @@ const routes = [
 
 ]
 export const Navigation = () => {
+    const [isOpen,setisOpen] = useState(false);
+    const router = useRouter();
     const pathname = usePathname()
+    const isMobile = useMedia("(max-width:1024px)",false);
+
+    const onClick = (href:string)=>
+    {
+        router.push(href);
+        setisOpen(false);
+    };
+    if(isMobile)
+    {
+        return (
+            <Sheet open={isOpen} onOpenChange={setisOpen}>
+                <SheetTrigger>
+                    <Button
+                    variant="outline"
+                    size="sm"
+                    className="font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none
+                    focus-visible:ring-offset-0 focus-visible:ring-transparent
+                    outline-none text-white focus:bg-white/30 transition"
+                    
+                    >
+                        <Menu className="size-4" />
+                        
+                    </Button>
+                </SheetTrigger>
+                <SheetContent side="left" className="px-2">
+                    <nav className="flex flex-col gap-y-2 pt-6">
+                        {routes.map((route)=>(
+                            <Button
+                            variant={route.href==pathname?"secondary":"ghost"}
+                            
+                            key={route.href}
+                            onClick={()=>onClick(route.href)}
+                            className="2-full justify-start"
+                            >
+                            
+                                {route.label}
+
+                            </Button>
+                        ))}
+                    </nav>
+                </SheetContent>
+            </Sheet>
+        )
+    }
+
+    
     return (
         <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
             {routes.map((route) => {
