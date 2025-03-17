61d9cb17dfcd841c2efbdfd67b86288de2a55ed2
Muhammad-Hamza-Khan-03 <163638005+Muhammad-Hamza-Khan-03@users.noreply.github.com>
2025-03-17 17:15:55 +0500

    Create groq.ts

---
+++ src/lib/utils/groq.ts
@@ -0,0 +1,18 @@
+import Groq from "groq-sdk";
+
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+export const reqGroqAI = async (content: any) => {
+  const res = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content,
+      },
+    ],
+    model: "deepseek-r1-distill-llama-70b",
+  });
+  return res;
+};
